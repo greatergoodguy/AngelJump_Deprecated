90ca7e18a52d77d80672e58070e5a5d486310dae
Tom Yu <tom.yu@burstingbrains.com>
2015-01-13 22:23:49 -0600

    Stable commit

---
+++ Assets/Angel Jump/Scripts/Sync/SyncAnimator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PhotonView))]
+public class SyncAnimator : Photon.MonoBehaviour {
+
+	Animator animator;
+	int animation;
+
+	void Awake() {
+		animator = GetComponent<Animator>();
+	}
+
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.isWriting) {
+			stream.SendNext(animator.GetInteger("Animation"));
+		}
+		else {
+			animation = (int)stream.ReceiveNext();
+		}
+	}
+
+	void Update() {
+		if (!photonView.isMine) {
+			animator.SetInteger("Animation", animation);
+		}
+	}
+
+
+}
