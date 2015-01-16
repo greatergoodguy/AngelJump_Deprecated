70a411358e8d760c2b0b630f7eaccb5fd651b84c
Tom Yu <tom.yu@burstingbrains.com>
2015-01-15 22:03:50 -0600

    Stable ccommit

--- Assets/Angel Jump/Scripts/MB God/GodLogger.cs
+++ Assets/Angel Jump/Scripts/MB God/GodLogger.cs
@@ -9,15 +9,22 @@ public class GodLogger : God_Base {
 	public bool logMasterScript 	= false;
 	public bool logAngel 			= false;
 
+	bool logAllClone;
 	bool logMasterScriptClone;
 	bool logAngelClone;
 
 	void Awake() {
+		logAllClone = logAll;
 		logMasterScriptClone = logMasterScript;
 		logAngelClone = logAngelClone;
 	}
 
 	void Update() {
+		if(logAllClone != logAll) {
+			logAllClone = logAll;
+
+		}
+
 		if(logMasterScriptClone != logMasterScript) {
 			logMasterScriptClone = logMasterScript;
 			UtilLogger.SetLoggableMasterScript(logMasterScript);
